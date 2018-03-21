2438132815fdfa090c91a5e4ae5d6fc5dd04b937
guizheng <guizheng@iflytek.com>
2018-03-21 19:50:37 +0800

    first commit

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,15 +3,25 @@ require('styles/App.css');
 
 import React from 'react';
 
-let yeomanImage = require('../images/yeoman.png');
+// 获取图片相关的数据
+let imageDatas = require('../data/imageDatas.json');
+
+imageDatas = (function genImageURL(imageDataArr) {
+  for (let i = 0, j = imageDataArr.length; i < j; i++) {
+    let singleImageData = imageDataArr[i];
+    singleImageData.imageURL = require('../images/' + singleImageData.fileName);
+    imageDataArr[i] = singleImageData;
+  }
+  return imageDataArr;
+})(imageDatas)
 
 class AppComponent extends React.Component {
   render() {
     return (
-      <div className="index">
-        <img src={yeomanImage} alt="Yeoman Generator" />
-        <div className="notice">Please edit <code>src/components/Main.js</code> to get started!</div>
-      </div>
+      <section className="stage">
+        <section className="img-sec"></section>
+        <nav className="controller-nav"></nav>
+      </section>
     );
   }
 }
