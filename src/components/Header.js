6e805dacdb7a73a88f8188d7bd62148e5dbfb18e
guizheng <guizheng@iflytek.com>
2018-03-27 21:44:04 +0800

    首页文章列表

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,30 +5,35 @@ import React from 'react';
 
 
 class Header extends React.Component {
-  constructor (props) {
-    super(props);
+    constructor(props) {
+        super(props);
+    }
 
-  }
+    render() {
+        let logoSrc = require('../images/syhs.png');
+        let labels = require('../data/labels.json');
+        var lableItemts = labels.map((label) =>
+            <div className="label">
+                <span>{label.labelName}</span>&nbsp;|&nbsp;
+            </div>
+        );
+        return (
+            <div className="header">
+                <div className="logo">
+                    <img src={logoSrc}/>
+                </div>
 
-  render () {
-    let logoSrc = require('../images/syhs.png')
-    return (
-      <div className="header">
-        <div className="logo">
-          <img src={logoSrc}/>
-        </div>
+                <div className="nav">
+                    {lableItemts}
+                </div>
 
-        <div className="nav">
-
-        </div>
-
-        <div className="login">
-          <span>登录</span>&nbsp;|&nbsp;
-          <span>注册</span>
-        </div>
-      </div>
-    );
-  }
+                <div className="login">
+                    <span>登录</span>&nbsp;|&nbsp;
+                    <span>注册</span>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default Header;
